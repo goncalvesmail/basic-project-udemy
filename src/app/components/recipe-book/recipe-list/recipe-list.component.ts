bf4d1214fee862ccdb2ae2cf5362ba3a294cf855
unknown <goncalvesmail@gmail.com>
2018-03-25 16:18:53 -0300

    Commit realizado ao fim da aula 48

--- src/app/components/recipe-book/recipe-list/recipe-list.component.ts
+++ src/app/components/recipe-book/recipe-list/recipe-list.component.ts
@@ -1,11 +1,19 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Recipe } from '../../../models/recipe';
+
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe('Receita teste','Apenas um testes para ver se esta ok',
+    'https://abrilclaudia.files.wordpress.com/2017/07/receita-talharim-abobrinha.jpg?quality=85&strip=info'),
+    new Recipe('Coxinha','Como fazer uma coxinha de galinha',
+    'https://www.comidaereceitas.com.br/images/stories/2013/10/coxinha_creme_galinha.jpg')
+  ];
 
   constructor() { }
 
