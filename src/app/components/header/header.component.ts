628de49e007c643a3b80ce3973e5094c7eb583bb
Daniel Goncalves de Oliveira <daniel.goncalves-oliveira@serpro.gov.br>
2018-03-27 17:03:01 -0300

    Commit após aula 79

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -7,9 +7,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
+  @Output()
+  onChangeMenu = new EventEmitter<string>();
   constructor() { }
 
   ngOnInit() {
   }
 
+  shoppinglistClicked() {
+    this.onChangeMenu.emit('listMenu');
+  }
+
+  recipeClicked() {
+    this.onChangeMenu.emit('recipeMenu');
+  }
 }
